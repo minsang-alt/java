fa2a8bae0f4868904ce3d14b77e0093936d32af4
minsang-alt <mazinger97@naver.com>
2023-12-25 00:38:57 +0900

    feat: 리트코드 문제 풀이

--- src/main/java/com/example/javaspring/codingTest/leetcode/TwoSum.java
+++ src/main/java/com/example/javaspring/codingTest/leetcode/TwoSum.java
@@ -1,4 +1,29 @@
 package com.example.javaspring.codingTest.leetcode;
 
+import java.util.Arrays;
+
 public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+
+        int[] ints = twoSum.twoSum(new int[]{3, 3}, 6);
+        System.out.println("ints = " + Arrays.toString(ints));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left != right) {
+            if (nums[left] + nums[right] < target) {
+                left += 1;
+            } else if (nums[left] + nums[right] > target) {
+                right -= 1;
+            } else {
+                return new int[]{left, right};
+            }
+        }
+        return null;
+    }
 }
